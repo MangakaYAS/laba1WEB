965b6713c3f7a3b5b28cae822883964e4655e633
MangakaYAS <46774954+MangakaYAS@users.noreply.github.com>
2019-06-03 13:27:31 +0500

    Add files via upload

---
+++ labanomer1/index.php
@@ -0,0 +1,31 @@
+﻿<HTML>
+<HEAD>
+<meta charset="utf-8">
+<TITLE>Lab1</TITLE>
+</HEAD>
+
+<?php
+echo "<div class='center'></div>";
+echo "<link rel='stylesheet' type='text/css' href='css/style.css'/>";
+?>
+
+<?php
+echo "<div class='main'></div><div class='main2'></div>";
+echo "<div class='right'>";
+?>
+
+<BODY>
+
+
+
+<?php
+echo "</div>";
+?>
+
+
+
+<?php
+echo "</div>";
+?>
+</BODY>
+</HTML>
